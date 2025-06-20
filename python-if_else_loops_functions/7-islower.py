b12360788988e652c7261f714538be87dfe06473
HenriquePAR <hpa.rodr@gmail.com>
2025-06-20 19:37:17 +0000

    consertando 7-ioslower.py

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+
 def islower(c):
     for i in range(97,123):
         if ord(c) == i:
-           return "{} is lower".format(c)
-    else:
-        return "{} is upper".format(c)
-print(islower('"'))
-print(islower('a'))
-print(islower('H'))
-print(islower('4'))
-print(islower('!'))
+           return True
+    return False
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("A is {}".format("lower" if islower("A") else "upper"))
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("g is {}".format("lower" if islower("g") else "upper"))
+
